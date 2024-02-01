fb1cabbef1e52dd168862438c9c77023d91ee3b7
Joy <github.joysaha@gmail.com>
2024-02-01 22:43:55 +0600

    last commit push

--- for-loop.js
+++ for-loop.js
@@ -6,11 +6,25 @@
 //     i++;
 // }while(i>10);
 
-let arr = [34, 3, 56,78, 90,332, 876,45];
+// let arr = [34, 3, 56,78, 90,332, 876,45];
 
-for (let item of arr){
-    if (item % 2 !== 0){
-        continue;
-    }
-    console.log(item);
+// for (let item of arr){
+//     if (item % 2 !== 0){
+//         continue;
+//     }
+//     console.log(item);
+// }
+
+function updateCountdown(seconds) {
+    console.log(seconds)
+}
+
+for (let i = 81; i>=65; i--){
+    setTimeout(() => {
+        updateCountdown(i);
+
+        if (i === 65) {
+            console.log('countdown complete');
+        }
+    }, (81 - i) * 1000);
 }
